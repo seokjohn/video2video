08e8e64f29ea308a5a0c3c096e63f5029d87b0ca
seok yo han <57163202+seokjohn@users.noreply.github.com>
2024-12-24 23:33:18 +0900

    Update FFmpeg.vue

--- src/components/FFmpeg.vue
+++ src/components/FFmpeg.vue
@@ -68,7 +68,7 @@ export default defineComponent({
       await ffmpeg.load({
         coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
         wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-        //workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript'),
+        workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript'),
       })
 
       message.value = 'Start transcoding'
