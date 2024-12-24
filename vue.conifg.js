97e56241a0891852aad32a100bc45393d21c0c6d
seok yo han <57163202+seokjohn@users.noreply.github.com>
2024-12-24 22:30:33 +0900

    Update vue.conifg.js

--- vue.conifg.js
+++ vue.conifg.js
@@ -1,4 +1,11 @@
+const path = require('path');
+
 module.exports = {
-    publicPath: '/video2video/',
-    outputDir: 'docs'
-}
+  publicPath: '/video2video/',
+  outputDir: 'docs', 
+  configureWebpack: {
+    output: {
+      publicPath: '/video2video/',
+    },
+  },
+};
